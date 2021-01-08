d82c33f589600c7af0935e6b62a31ef21dcc1a6d
Matheus Campos <matheusfreitascampos@gmail.com>
2021-01-08 18:44:23 -0300

    3 Challenge

---
+++ Challenges.py
@@ -0,0 +1,18 @@
+# 1-Challenge
+#   nome = input('Qual é o seu nome?')
+#
+#   print('Olá', nome, 'Prazer em te conhecer!')
+
+# 2-Challenge
+#   dia = input ('Dia =')
+#   mes = input ('Mês =')
+#   ano = input ('Ano =')
+
+#   print('você nasceu', dia,'/',mes,'/',ano)
+
+# 3-Challenge
+
+num1 = input ('Me diga um numero =') 
+num2 = input ('Me diga mais um numero =')
+
+print(num1 + num2)
