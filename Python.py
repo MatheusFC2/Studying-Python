7c3efec0221f9e4497eacefa34005747348b4e30
Matheus Campos <matheusfreitascampos@gmail.com>
2021-01-08 18:43:57 -0300

    commands and learning, string, number, var

---
+++ Python.py
@@ -0,0 +1,17 @@
+# String
+#   $print ('Olá, Mundo')
+
+# Numbers
+#   $print(5 + 8)
+
+# Var
+#   nome = 'Matheus'
+#   idade = 15
+#   peso = 50
+#   $print(nome,idade,peso)
+
+# Using input
+#   nome = input ('Qual é seu nome?')
+#   idade = input ('Quantos anos você tem?')
+#   peso = input ('Qual é o seu peso?')
+#   $print(nome, idade, peso)
