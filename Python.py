127a6c1379296479d14778fbf74cee2821b08134
Matheus Campos <matheusfreitascampos@gmail.com>
2021-01-09 22:46:21 -0300

    Adicionei novo item .format

--- Python.py
+++ Python.py
@@ -15,3 +15,10 @@
 #   idade = input ('Quantos anos você tem?')
 #   peso = input ('Qual é o seu peso?')
 #   $print(nome, idade, peso)
+
+# New Sintax, using .format
+#   n1 = int(input('Digite um numero: ')) 
+#   n2 = int(input('Digite mais um numero: '))
+#   s = n1 + n2
+#   print('A soma entre {} e {} é igual a {}'.format(n1, n2, s))  
+
