7a99c33ccf74c6a17f8e006e152ef1d17b3fa6b7
Matheus Campos <matheusfreitascampos@gmail.com>
2021-01-09 22:45:37 -0300

    Adicionei mais 2 Desafio e nova sintaxe .format

--- Challenges.py
+++ Challenges.py
@@ -21,3 +21,9 @@
 #   n2 = int(input('Digite mais um numero: '))
 #   s = n1 + n2
 #   print('A soma entre', n1, 'e', n2, 'é igual a', s)  
+# -------------------------------------------------
+#  3.3 Challenge usando nova sintaxe com .format
+#    n1 = int(input('Digite um numero: ')) 
+#   n2 = int(input('Digite mais um numero: '))
+#   s = n1 + n2
+#   print('A soma entre {} e {} é igual a {}'.format(n1, n2, s))  
